eb0383848ce38f3c68e342066463e5713f046846
Rezaadi38 <rezapratama381@smk.belajar.id>
2022-12-20 17:26:21 +0700

    menambahkan file rest

---
+++ rest.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.ok = function(values, rest){
+    var data = {
+        'status':200,
+        'values':values
+
+    };
+      
+      rest.json(data);
+      rest.end();
+}
