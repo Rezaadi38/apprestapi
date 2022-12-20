23c8e09bfbd2d15c342ca735b131a02984202180
Rezaadi38 <rezapratama381@smk.belajar.id>
2022-12-20 21:00:31 +0700

    enambahkan tampil mahasiswa di controller js

--- controller.js
+++ controller.js
@@ -5,4 +5,15 @@ var connection = require('./koneksi');
 
 exports.index = function(req,rest){
     response.ok("aplikasi REST API ku berjalan!",rest)
-};          
+};      
+
+//menamppikan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,rest){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+       if(error){
+        connection.log(error);
+       }else {
+        response.ok(rows, rest)
+       }        
+    });
+};
