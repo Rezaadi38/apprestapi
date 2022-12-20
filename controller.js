7459b4bb7c7ad32a0f8dc1f3acd75ae14a6dc05d
Rezaadi38 <rezapratama381@smk.belajar.id>
2022-12-20 19:40:46 +0700

    emperbarui controller js

--- controller.js
+++ controller.js
@@ -3,6 +3,6 @@
 var response = require('./rest');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
+exports.index = function(req,rest){
     response.ok("aplikasi REST API ku berjalan!",rest)
 };          
